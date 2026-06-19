b02362e591ed98ace752b5324db56122298193aa
GodAzrail <godartstyle@gmail.com>
2026-06-19 23:19:31 +0700

    Delete tariff-tools-patch.js

--- tariff-tools-patch.js
+++
@@ -1,6 +0,0 @@
-// tariff-tools-patch.js
-// Патч больше не нужен: дедупликация логов перенесена в tariff-tools-content.js.
-(function () {
-  'use strict';
-  console.log('[TariffToolsPatch] Skip: built-in log deduplication is active');
-})();
